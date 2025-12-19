520df5aee844cb5afc4882c07aca797ddb59b1a6
Omer Faruk Acar <acarr.omerfaruk@gmail.com>
2025-12-19 19:50:35 +0300

    Proje kurulumu: STM32 USB HID Media Controller altyapisi

---
+++ Src/main.c
@@ -0,0 +1,508 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include "usb_device.h"
+#include "gpio.h"
+
+/* USER CODE BEGIN Includes */
+ #include "usbd_hid.h"
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+struct mouse_report_t
+{
+    uint8_t buttons;
+    int8_t x;
+    int8_t y;
+} mice_report;
+
+
+struct keyboard_report_t
+{
+
+    uint8_t id;
+
+    uint8_t modifier;
+    uint8_t reserved;
+    uint8_t keycode[6];
+} kbd_report;
+
+
+#define SHIFT 0x80
+const uint8_t _asciimap[128] =
+{
+    0x00,             // NUL
+    0x00,             // SOH
+    0x00,             // STX
+    0x00,             // ETX
+    0x00,             // EOT
+    0x00,             // ENQ
+    0x00,             // ACK
+    0x00,             // BEL
+    0x2a,           // BS   Backspace
+    0x2b,           // TAB  Tab
+    0x28,           // LF   Enter
+    0x00,             // VT
+    0x00,             // FF
+    0x00,             // CR
+    0x00,             // SO
+    0x00,             // SI
+    0x00,             // DEL
+    0x00,             // DC1
+    0x00,             // DC2
+    0x00,             // DC3
+    0x00,             // DC4
+    0x00,             // NAK
+    0x00,             // SYN
+    0x00,             // ETB
+    0x00,             // CAN
+    0x00,             // EM
+    0x00,             // SUB
+    0x00,             // ESC
+    0x00,             // FS
+    0x00,             // GS
+    0x00,             // RS
+    0x00,             // US
+
+    0x2c,          //  ' '
+    0x1e|SHIFT,    // !
+    0x34|SHIFT,    // "
+    0x20|SHIFT,    // #
+    0x21|SHIFT,    // $
+    0x22|SHIFT,    // %
+    0x24|SHIFT,    // &
+    0x34,          // '
+    0x26|SHIFT,    // (
+    0x27|SHIFT,    // )
+    0x25|SHIFT,    // *
+    0x2e|SHIFT,    // +
+    0x36,          // ,
+    0x2d,          // -
+    0x37,          // .
+    0x38,          // /
+    0x27,          // 0
+    0x1e,          // 1
+    0x1f,          // 2
+    0x20,          // 3
+    0x21,          // 4
+    0x22,          // 5
+    0x23,          // 6
+    0x24,          // 7
+    0x25,          // 8
+    0x26,          // 9
+    0x33|SHIFT,      // :
+    0x33,          // ;
+    0x36|SHIFT,      // <
+    0x2e,          // =
+    0x37|SHIFT,      // >
+    0x38|SHIFT,      // ?
+    0x1f|SHIFT,      // @
+    0x04|SHIFT,      // A
+    0x05|SHIFT,      // B
+    0x06|SHIFT,      // C
+    0x07|SHIFT,      // D
+    0x08|SHIFT,      // E
+    0x09|SHIFT,      // F
+    0x0a|SHIFT,      // G
+    0x0b|SHIFT,      // H
+    0x0c|SHIFT,      // I
+    0x0d|SHIFT,      // J
+    0x0e|SHIFT,      // K
+    0x0f|SHIFT,      // L
+    0x10|SHIFT,      // M
+    0x11|SHIFT,      // N
+    0x12|SHIFT,      // O
+    0x13|SHIFT,      // P
+    0x14|SHIFT,      // Q
+    0x15|SHIFT,      // R
+    0x16|SHIFT,      // S
+    0x17|SHIFT,      // T
+    0x18|SHIFT,      // U
+    0x19|SHIFT,      // V
+    0x1a|SHIFT,      // W
+    0x1b|SHIFT,      // X
+    0x1c|SHIFT,      // Y
+    0x1d|SHIFT,      // Z
+    0x2f,          // [
+    0x31,          // bslash
+    0x30,          // ]
+    0x23|SHIFT,    // ^
+    0x2d|SHIFT,    // _
+    0x35,          // `
+    0x04,          // a
+    0x05,          // b
+    0x06,          // c
+    0x07,          // d
+    0x08,          // e
+    0x09,          // f
+    0x0a,          // g
+    0x0b,          // h
+    0x0c,          // i
+    0x0d,          // j
+    0x0e,          // k
+    0x0f,          // l
+    0x10,          // m
+    0x11,          // n
+    0x12,          // o
+    0x13,          // p
+    0x14,          // q
+    0x15,          // r
+    0x16,          // s
+    0x17,          // t
+    0x18,          // u
+    0x19,          // v
+    0x1a,          // w
+    0x1b,          // x
+    0x1c,          // y
+    0x1d,          // z
+    0x2f|SHIFT,    // {
+    0x31|SHIFT,    // |
+    0x30|SHIFT,    // }
+    0x35|SHIFT,    // ~
+    0               // DEL
+};
+
+
+int scan_cvt=1;
+void kbd_send_ch(uint8_t ch){
+    int code;
+    int i;
+    if( scan_cvt ){
+        if( ch > 128 ){
+            ch -=128;
+        }
+        code = _asciimap[ch];
+    }
+    else
+        code = ch;
+
+
+    //id set here must be consistent with the one in Hid report descriptor
+    //    0x85, 0x01,        //   Report ID (1)
+    kbd_report.id = 1;
+
+    kbd_report.keycode[0]=code&0x7F;
+    kbd_report.keycode[1]=0;
+    if ( code & 0x80) {                  // it's a capital letter or other character reached with shift
+        kbd_report.modifier |= 0x02;    // the left shift modifier
+    }
+    for( i=0; i< 4;i++){
+        USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t *)&kbd_report, sizeof(kbd_report));
+        HAL_Delay(10);
+    }
+    memset(kbd_report.keycode, 0 , sizeof(kbd_report.keycode));
+    kbd_report.modifier = 0;
+    USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t *)&kbd_report, sizeof(kbd_report));
+    HAL_Delay(100);
+
+}
+
+void kbd_send_str(const char *str){
+    while( *str){
+        kbd_send_ch(*str);
+        str++;
+    }
+}
+
+
+#define HID_MEDIA_PAUSE  0xB1 // pause
+#define HID_MEDIA_RECORD  0xB3
+#define HID_MEDIA_SCAN_NEXT 0xB5
+#define HID_MEDIA_SCAN_PREV 0xB6
+#define HID_MEDIA_STOP  0xB7
+#define HID_MEDIA_EJECT 0xB8
+#define HID_MEDIA_VOL_UP 0xE9
+#define HID_MEDIA_VOL_DONW 0xEA
+#define HID_MEDIA_PLAY  0xCD // play/pause
+
+
+void  kbd_vol_up(){
+    uint8_t report[3];
+    report[0]= HID_MEDIA_REPORT;
+    report[1]= 0xE9;
+    report[2]= 0x00;
+    USBD_HID_SendReport(&hUsbDeviceFS, report, 3);
+    HAL_Delay(30);
+
+    report[0]= HID_MEDIA_REPORT;
+    report[1]= 0x00;
+    report[2]= 0x00;
+    USBD_HID_SendReport(&hUsbDeviceFS, report, 3);
+
+}
+
+
+
+void  kbd_vol_down(){
+    uint8_t report[3];
+    report[0]= HID_MEDIA_REPORT;
+
+    report[1]= HID_MEDIA_VOL_DONW;
+
+    report[2]= 0x00;
+    USBD_HID_SendReport(&hUsbDeviceFS, report, 3);
+    HAL_Delay(30);
+
+    report[0]= HID_MEDIA_REPORT;
+    report[1]= 0x00;
+    report[2]= 0x00;
+    USBD_HID_SendReport(&hUsbDeviceFS, report, 3);
+
+}
+
+void kbd_do_media(int code){
+    uint8_t report[3];
+    report[0]= HID_MEDIA_REPORT;
+    report[1]= code;
+    report[2]= 0x00;
+    USBD_HID_SendReport(&hUsbDeviceFS, report, 3);
+    HAL_Delay(30);
+
+    report[0]= HID_MEDIA_REPORT;
+    report[1]= 0x00;
+    report[2]= 0x00;
+    USBD_HID_SendReport(&hUsbDeviceFS, report, 3);
+    HAL_Delay(20);
+
+}
+
+
+void USBD_HID_GetReport(uint8_t * report, int len){
+    // see from http://www.microchip.com/forums/m433757.aspx
+    // report[0] is the report id
+    // report[1] is the led bit filed
+    // D0: NUM lock
+    // D1: CAPS lock
+    // D2: SCROLL lock
+    // D3: Compose
+    // D4: Kana
+    if ( report[0]==1 ){ // report id 1 is "led" refer to report id in hid report des
+        HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin , report[1]&0x01? GPIO_PIN_SET: GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin , report[1]&0x02? GPIO_PIN_SET: GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin , report[1]&0x04? GPIO_PIN_SET: GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin , report[1]&0x08? GPIO_PIN_SET: GPIO_PIN_RESET);
+    }
+
+}
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+    int send=0;
+    char str[10]="test";
+    int code=0;
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USB_DEVICE_Init();
+
+  /* USER CODE BEGIN 2 */
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+      if( send){
+           switch (send ) {
+           case 1 :
+               kbd_send_str(str);
+               break;
+           case 2 :
+               kbd_vol_down();
+               break;
+           case 3 :
+               kbd_vol_up();
+               break;
+           case 4:
+               kbd_do_media(code);
+           break;
+           }
+           HAL_Delay(1000);
+           send = 0;
+
+       }
+  
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
